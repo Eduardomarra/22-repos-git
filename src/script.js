c2c87778e49dc40c28f64aa9acb8da07fc24404d
Eduardo Marra <eduardomarra@gmail.com>
2022-04-12 13:14:37 -0300

    finished project

---
+++ src/script.js
@@ -0,0 +1,52 @@
+const date = document.querySelector('.container p a span')
+const year = new Date().getFullYear()
+date.innerHTML = `${year}`
+
+function getApiGithub() {
+    const cards = document.querySelector(".cards")
+    fetch("https://api.github.com/users/eduardomarra/repos")
+        .then(async res => {
+            const data = await res.json();
+
+            data.map(item => {
+                let cardItem = document.createElement('div')
+                cardItem.setAttribute('class', 'card-item')
+
+                cardItem.innerHTML = `
+                    <div class="imgProject">
+                        <img src="./assets/imgProject.jpg" alt="">
+                    </div>
+                    <h2>${item.name}</h2>
+                    <p>${item.description}</p>
+                    <div class="btn">
+                        <button class="btn-demo">
+                            <a href="${item.html_url}">Demo</a>
+                        </button>
+                        <button class="btn-github">
+                            <a href="${item.html_url}">GitHub</a>
+                        </button>
+                    </div>
+                `
+                cards.appendChild(cardItem)
+            })
+        })
+}
+
+const cards = document.querySelector('.container.show');
+const showProject = document.querySelector('.show-project');
+const inner = "Ver menos"
+const inner2 = "Ver mais"
+console.log(showProject)
+
+showProject.addEventListener("click", openCards)
+
+function openCards() {
+    cards.classList.toggle("show");    
+    if(showProject.innerHTML === 'Ver mais'){
+        showProject.innerHTML = `Ver menos`
+    }else{
+        showProject.innerHTML = `Ver mais`
+    }
+}
+
+getApiGithub()
